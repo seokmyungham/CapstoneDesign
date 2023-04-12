0cbd29e228192cda090b0876c4dce08f78c219b5
seokmyungham <hellotjraud@gmail.com>
2023-04-12 21:30:57 +0900

    MySQL57 deprecated 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,7 +7,7 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=hello-spring
+PROJECT_NAME=capstone
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
